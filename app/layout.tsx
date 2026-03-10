635dfccb0c1e86a25791501cfeae73a28a3a5feb
ABLonq <54056423+ABLonq@users.noreply.github.com>
2026-03-10 12:47:14 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '💕 Anılarımız',
+  title: "S💕B'nin Anıları",
   description: 'Seninle geçirdiğimiz her an...',
 }
 
